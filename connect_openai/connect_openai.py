58ceb0382f9ee33719e5683d894c5f85e81ac80a
Philipp Warmer <philipp.warmer@d-one.ai>
2023-03-25 22:13:20 +0100

    added personality

--- connect_openai/connect_openai.py
+++ connect_openai/connect_openai.py
@@ -1,25 +1,5 @@
 import openai
 import os
-from dotenv import load_dotenv
-
-# Load your API key from an environment variable or secret management service
-load_dotenv()
-
-openai.api_key = os.getenv("API_KEY")
-
-
-def use_llm(prompt: str):
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": prompt}],
-    )
-    return completion
-
-
-#ans = use_llm("What is the role of glutamine in Hela cells")
-#print(ans)
-import openai
-import os
 
 # Set up the OpenAI API key
 openai.api_key = os.environ["OPENAI_API_KEY"]
@@ -30,9 +10,30 @@ conversation_history = []
 def add_message_to_history(user, message):
     conversation_history.append({"role": user, "content": message})
 
+def generate_system_message(prompt):
+    personality = f"""You are science.pal a researcher specialized in life science. 
+                     You are an expert in wet and dry lab practices in particular in 
+                     the whole field of {microbiology} research"""
+    keyword_contexts = {
+        "onco": "oncology",
+        "microbiology": "microbiology",
+        "systemsbiology": "systemsbiology",
+        "cancer": "cancer",
+    }
+
+    for keyword, context in keyword_contexts.items():
+        if keyword in prompt.lower():
+            return context
+
+    return personality
+
 def chatbot_response(prompt):
     # Add the prompt to the conversation history
     add_message_to_history("user", prompt)
+
+    # Generate system message based on keywords
+    system_message = generate_system_message(prompt)
+    add_message_to_history("system", system_message)
 
     # Prepare input for the API by combining the conversation history
     input_messages = [{"role": message["role"], "content": message["content"]} for message in conversation_history]
