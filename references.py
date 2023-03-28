c888a5dfb15821ec6ea6ee8f404dc3bac5c25e79
Philipp Warmer <philipp.warmer@d-one.ai>
2023-03-28 13:38:12 +0200

    updated data selection

--- references.py
+++ references.py
@@ -18,8 +18,8 @@ def add_references(ans:str, top_n:int):
 
 def add_ref(ans: str, top_n:int):
     ans_embeddings = calc_embeddings(ans, 'bert')
-    abs_embeddings = get_data('bio_axv_embeddings')
+    abs_embeddings = get_data('bio_axv_embeddings_microbiology')
     similarities = distance_between_vector_and_vectors(ans_embeddings, abs_embeddings)
-    abstracts = get_data('bio_axv_preprints')
+    abstracts = get_data('bio_axv_preprints_microbiology')
     top_id = (-similarities).argsort()[:top_n]
     return render_abstract_ranking(abstracts, top_id)
