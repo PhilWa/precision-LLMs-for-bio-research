abbd5e7c02eacea5512ceda2ca88b1b8d4bca41f
Philipp Warmer <philipp.warmer@d-one.ai>
2023-03-29 14:30:01 +0200

    formatting

--- connect_openai/connect_openai.py
+++ connect_openai/connect_openai.py
@@ -9,8 +9,10 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 # Initialize conversation history
 conversation_history = []
 
+
 def add_message_to_history(user, message):
     conversation_history.append({"role": user, "content": message})
+
 
 def generate_system_message(prompt):
 
@@ -23,31 +25,32 @@ def generate_system_message(prompt):
 
     for keyword, context in keyword_contexts.items():
         if keyword in prompt.lower():
-            return f"""You are science.pal a researcher specialized in life science. 
+            return f"""You are science.pal a researcher specialized in life science.
                      You are an expert in wet and dry lab work in particular in 
-                     the field of {keyword}"""
+                     the field of {keyword}. Ask follow up questions after giving an concise answer"""
 
     return f"""You are science.pal a researcher specialized in life science. 
-                     You are an expert in wet and dry lab practices"""
+                     You are an expert in wet and dry lab practices. Ask follow up questions after giving an concise answer"""
 
 
 def config_model(prompt):
 
-    if 'action::ideate' in prompt.lower():
+    if "action::ideate" in prompt.lower():
         # TODO have a fun for prompt replace with logging.
-        prompt = prompt.replace('action::ideate', "")
+        prompt = prompt.replace("action::ideate", "")
         return prompt, 0.75
 
-    if 'action::plan' in prompt.lower():
-        prompt = prompt.replace('action::plan', "")
+    if "action::plan" in prompt.lower():
+        prompt = prompt.replace("action::plan", "")
         return prompt, 0.1
-    
+
     else:
         return prompt, 0.5
 
+
 def chatbot_response(prompt):
 
-    prompt, TEMPERATURE =config_model(prompt)
+    prompt, TEMPERATURE = config_model(prompt)
 
     # Add the prompt to the conversation history
     add_message_to_history("user", prompt)
@@ -57,7 +60,10 @@ def chatbot_response(prompt):
     add_message_to_history("system", system_message)
 
     # Prepare input for the API by combining the conversation history
-    input_messages = [{"role": message["role"], "content": message["content"]} for message in conversation_history]
+    input_messages = [
+        {"role": message["role"], "content": message["content"]}
+        for message in conversation_history
+    ]
 
     # Call the API
     response = openai.ChatCompletion.create(
@@ -69,8 +75,6 @@ def chatbot_response(prompt):
     )
 
     # Extract and return the response
-    reply = response.choices[0].message['content'].strip()
+    reply = response.choices[0].message["content"].strip()
     add_message_to_history("assistant", reply)
     return reply
-
-
