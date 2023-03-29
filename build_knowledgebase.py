bac180ca8b8e67b16657b35abaf935ac75915451
Philipp Warmer <philipp.warmer@d-one.ai>
2023-03-29 08:38:03 +0200

    refactored knowledgebase and added fts

--- build_knowledgebase.py
+++ build_knowledgebase.py
@@ -1,10 +1,51 @@
-from utils import write_dataframe_to_sqlite
-import pandas as pd
+from utils import build_knowledgebase
+
+### Metabolites and proteins have the common key PathBank ID,
+### In pathways the PathBank ID	is named SMPDB
 
 
-df = pd.read_csv('data/pathbank_all_metabolites.csv')
-DB_NAME = 'pathbank_all_metabolites'
-TABLE_NAME = 'pathbank_all_metabolites'
-write_dataframe_to_sqlite(df, DB_NAME, TABLE_NAME)
-del df
-print('------ Done ------')
+# Metabolite database
+COL_NAMES = [
+    "Metabolite_Name",
+    "HMDB_ID",
+    "KEGG_ID",
+    "ChEBI_ID",
+    "DrugBank_ID",
+    "CAS",
+    "Formula",
+]
+build_knowledgebase(
+    dir="data/pathbank_all_metabolites.csv",
+    db_name="collections.sqlite",
+    table_name="pathbank_all_metabolites",
+    column_names=COL_NAMES,
+)
+
+# Protein database
+COL_NAMES = [
+    "Uniprot_ID",
+    "Protein_Name",
+    "HMDBP_ID",
+    "DrugBank_ID",
+    "GenBank_ID",
+    "Gene_Name",
+    "Locus",
+]
+build_knowledgebase(
+    dir="data/pathbank_all_proteins.csv",
+    db_name="collections.sqlite",
+    table_name="pathbank_all_proteins",
+    column_names=COL_NAMES,
+)
+
+# Pathway database
+COL_NAMES = [
+    "pathway_name",
+    "Description",
+]
+build_knowledgebase(
+    dir="data/pathbank_pathways.csv",
+    db_name="collections.sqlite",
+    table_name="pathbank_pathways",
+    column_names=COL_NAMES,
+)
