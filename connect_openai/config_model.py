e0b7f32ba6ee6c04c5e0765bc5607e31656a8cb9
Philipp Warmer <philipp.warmer@d-one.ai>
2023-03-31 23:14:44 +0200

    adding dynamic model config

---
+++ connect_openai/config_model.py
@@ -0,0 +1,84 @@
+import re
+
+
+# Function to process the sentence and extract keywords
+def extract_keywords(sentence: str, groups: dict):
+    keywords = []
+    for group in groups.values():
+        for subgroup in group.values():
+            for keyword in subgroup:
+                if re.search(r"\b" + keyword + r"\b", sentence, re.IGNORECASE):
+                    keywords.append(keyword)
+    return keywords
+
+
+# Function to map extracted keywords to their respective groups and subgroups
+def map_keywords_to_groups(keywords):
+    groups = {
+        "model_temperature": {
+            ".1": ["protocol", "method"],
+            ".7": ["explore", "envision"],
+        },
+        "model_type": {
+            "gpt-3.5-turbo": [
+                "gpt-3.5",
+            ],
+            "gpt-4": [
+                "gpt-4",
+            ],
+        },
+        "biology": {
+            "microbiology": ["e.coli", "escherichia coli", "bacteria"],
+            "cancer": ["oncology", "tumor"],
+        },
+    }
+
+    mapped_groups = {}
+    for keyword in keywords:
+        for group_name, group in groups.items():
+            for subgroup_name, subgroup_keywords in group.items():
+                if keyword in subgroup_keywords:
+                    if group_name not in mapped_groups:
+                        mapped_groups[group_name] = {}
+                    if subgroup_name not in mapped_groups[group_name]:
+                        mapped_groups[group_name][subgroup_name] = []
+                    mapped_groups[group_name][subgroup_name].append(keyword)
+    return mapped_groups
+
+
+def map_words_to_config(sentence: str):
+
+    default_params = {
+        "model_temperature": 0.5,
+        "model_type": "gpt-3.5-turbo",
+        "biology": "general",
+    }
+
+    # Extract keywords and map them to groups and subgroups
+    keywords = extract_keywords(sentence)
+    # Define groups, subgroups, and keywords
+    # To be replaced with embeddings
+    mapped_groups = map_keywords_to_groups(keywords)
+    return get_model_params(mapped_groups, default_params)
+
+
+def get_model_params(mapped_groups, default_params):
+    for k, v in default_params.items():
+        if isinstance(mapped_groups.get(k), dict):
+            default_params[k] = next(iter(mapped_groups.get(k)))
+    return default_params
+
+
+def config_model(prompt):
+
+    if "action::ideate" in prompt.lower():
+        # TODO have a fun for prompt replace with logging.
+        prompt = prompt.replace("action::ideate", "")
+        return prompt, 0.75
+
+    if "action::plan" in prompt.lower():
+        prompt = prompt.replace("action::plan", "")
+        return prompt, 0.1
+
+    else:
+        return prompt, 0.5
