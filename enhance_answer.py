fbfa859b94c3e50792e71a7210aee0f58157e245
Philipp Warmer <philipp.warmer@d-one.ai>
2023-04-10 18:04:30 +0200

    rm deprecated funs

--- enhance_answer.py
+++ enhance_answer.py
@@ -2,45 +2,6 @@ import re
 import sqlite3
 from typing import Dict, List
 
-synonyms = [
-    "C3H7NO3",
-    "L-serine",
-    "serine",
-    "CHEBI:17115",
-    "(S)-2-Amino-3-hydroxypropanoic acid",
-    "(S)-Serine",
-]
-
-synonyms = [i.lower() for i in synonyms]
-
-
-def select_synonyms(word: str, synonyms: list[str], granularity: str = "all"):
-    """Deprecated"""
-    if granularity == "all":
-        return [i for i in synonyms if i.lower() != word.lower()]
-    else:
-        pass
-
-
-def enhance_prompt(words: str) -> str:
-    """Deprecated"""
-    pattern = r".*\baction::enhance\b.*"
-
-    if not re.match(pattern, words):
-        return words
-
-    ans = []
-    for word in words.split():
-        if word == "action::enhance":
-            continue
-        elif word.lower() in synonyms:
-            selected_synonyms = select_synonyms(word, synonyms)
-            ans += [word, f"(synonyms are: {selected_synonyms})"]
-        else:
-            ans += [word]
-
-    return " ".join(ans)
-
 
 def enrich_metabolite_information(
     sentence: str,
