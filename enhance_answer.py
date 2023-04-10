798129fc43716fe9151c21643784f0c4dba94820
Philipp Warmer <philipp.warmer@d-one.ai>
2023-04-10 17:41:07 +0200

    added metabolic knowledge

--- enhance_answer.py
+++ enhance_answer.py
@@ -1,4 +1,6 @@
 import re
+import sqlite3
+from typing import Dict, List
 
 synonyms = [
     "C3H7NO3",
@@ -13,6 +15,7 @@ synonyms = [i.lower() for i in synonyms]
 
 
 def select_synonyms(word: str, synonyms: list[str], granularity: str = "all"):
+    """Deprecated"""
     if granularity == "all":
         return [i for i in synonyms if i.lower() != word.lower()]
     else:
@@ -20,6 +23,7 @@ def select_synonyms(word: str, synonyms: list[str], granularity: str = "all"):
 
 
 def enhance_prompt(words: str) -> str:
+    """Deprecated"""
     pattern = r".*\baction::enhance\b.*"
 
     if not re.match(pattern, words):
@@ -38,8 +42,60 @@ def enhance_prompt(words: str) -> str:
     return " ".join(ans)
 
 
-import sqlite3
-from typing import Dict, List
+def enrich_metabolite_information(
+    sentence: str,
+    db_name: str = "collections.sqlite",
+    fts_table: str = "pathbank_all_metabolites_fts",
+):
+    """
+    Enriches a given sentence with metabolite information if it contains the 'action::enhance' pattern.
+    """
+
+    pattern = r".*\baction::enhance\b.*"
+
+    if not re.match(pattern, sentence):
+        return sentence
+    sentence = sentence.replace("action::enhance", "")
+    print(sentence)
+    word_value_pairs = find_words_in_fts(sentence, db_name, fts_table)
+    return knowledge_to_prompt(word_value_pairs)
+
+
+def escape_fts5_special_chars(search_term: str) -> str:
+    """
+    Escapes FTS5 special characters in the given search term by surrounding each special character with double quotes.
+    """
+    fts5_special_chars = {
+        "(",
+        ")",
+        "{",
+        "}",
+        "[",
+        "]",
+        '"',
+        "^",
+        "-",
+        "|",
+        "@",
+        "~",
+        "&",
+        "*",
+        ":",
+        ".",
+    }
+
+    # Check if any special characters are present in the search term
+    if not any(char in search_term for char in fts5_special_chars):
+        return search_term
+
+    escaped_search_term = ""
+    for char in search_term:
+        if char in fts5_special_chars:
+            escaped_search_term += f'"{char}"'
+        else:
+            escaped_search_term += char
+
+    return escaped_search_term
 
 
 def find_words_in_fts(
@@ -47,7 +103,7 @@ def find_words_in_fts(
 ) -> Dict[str, List[Dict]]:
     # We have two challenges:
     # 1. Go through each word in sentence to find molecule and thus id and pythway info
-    # TODO We are missing the molecule id
+    # TODO Add missing project
     # 2. Take a sentence and match all model organisms.
     # Take the respective main group e.g. bacteria for e.coli
     # use it to limit the search space in fts table
@@ -60,12 +116,12 @@ def find_words_in_fts(
     word_results = {}
 
     for word in words:
-        print(word)
+        search_term = escape_fts5_special_chars(word)
         cursor.execute(
             f"""
         SELECT * FROM {fts_table} WHERE {fts_table} MATCH ?
         """,
-            (word,),
+            (search_term,),
         )
         results = cursor.fetchall()
         column_names = [desc[0] for desc in cursor.description]
@@ -74,8 +130,8 @@ def find_words_in_fts(
         ignore_list = [
             "and",
         ]
-        if word in ignore_list:
-            word_results[word] = [{}]
+        if word in ignore_list or len(word) <= 4:
+            word_results[word] = ""
 
         elif formatted_results:
             # Without subsetting by organism formatted_results is too large
@@ -89,15 +145,14 @@ def find_words_in_fts(
 
 
 def knowledge_to_prompt(word_value_pairs):
-    # Consumes output of find_words_in_fts
-    # injects them into prompt
+    # Consumes output of find_words_in_fts injects them into prompt
     ans = ""
     for k, v in word_value_pairs.items():
         ans += k + " "
         if isinstance(v, list) and len(v) == 1 and v[0] == {}:
             continue
-        else:
-            temp = "with synonyms "
+        elif isinstance(v, dict):  # Check if v is a dictionary
+            temp = ", also referred to as: "
             for kk, vv in v.items():
                 temp += kk + " : " + str(vv) + ", "
             ans += temp
